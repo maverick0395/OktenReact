ef5fd675f522bc4ef8cd47da7d40d5837671a5c7
Yaroslav <maverick032695@gmail.com>
2022-01-11 19:38:37 +0200

    remove condition in Launch.js

--- src/components/Launch/Launch.js
+++ src/components/Launch/Launch.js
@@ -2,7 +2,7 @@
 const Launch = (props) => {
     const {mission_name, launch_year, mission_patch} = props;
 
-    if (launch_year !== '2020') {
+
         return (
             <div className={'launch'}>
                 <div className={'left-side'}>
@@ -12,7 +12,6 @@ const Launch = (props) => {
                 <img src={mission_patch} alt='mission_patch'/>
             </div>
         );
-    }
 };
 
 export default Launch;
